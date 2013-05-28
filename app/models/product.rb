d61eaace0abb8e702604ea28637b4d7febff7e8c
batinhell <batinhell@gmail.com>
2013-05-28 18:43:35 +0400

    started creating shopping cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,17 @@ class Product < ActiveRecord::Base
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
   validates_length_of :title, :minimum => 5, :message => "more then 5 if you don't mind"
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Существуют товарные позиции')
+      return false
+    end
+  end
 end
