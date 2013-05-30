ecbfaf6577bc44966b948dc7ccd45df96f3190e4
batcat <batinhell@gmail.com>
2013-05-31 02:29:54 +0400

    add cart via ajax

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,6 @@ class StoreController < ApplicationController
   def index
     session[:counter].nil? ? session[:counter] = 1 : session[:counter] += 1
     @products = Product.order(:title)
+    @cart = current_cart
   end
 end
