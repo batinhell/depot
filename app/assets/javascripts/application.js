9c1a91544e97f23ae065cf86c5f7c412dbc5fa30
batinhell <batinhell@gmail.com>
2013-06-07 13:11:00 +0400

    add bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,4 @@
 //= require jquery_ujs
 //= require jquery.color.js
 //= require_tree .
+//= require bootstrap
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,6 @@
 class StoreController < ApplicationController
   skip_before_filter :authorize
   def index
-    session[:counter].nil? ? session[:counter] = 1 : session[:counter] += 1
     @products = Product.order(:title)
     @cart = current_cart
   end
